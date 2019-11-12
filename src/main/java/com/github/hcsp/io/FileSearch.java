c5c7b746aff8d898ccddaf17c6190ec5fd52696a
Figlure <kyrielight@sina.com>
2019-11-12 21:25:28 +0800

    commit 2 after mvn clean test

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -3,20 +3,19 @@ package com.github.hcsp.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) throws IOException {
+    public static int grep(File target, String text){
         if (!target.exists() || !target.canRead() || target.isDirectory()) {
             throw new IllegalArgumentException();
         }
         FileInputStream inputStream = null;
         Scanner sc = null;
-        int num=0;
+        int num=1;
         try {
             //InputStream
             inputStream = new FileInputStream(target);
@@ -24,10 +23,9 @@ public class FileSearch {
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 System.out.println(line);
-                if(line.contains(text)) {
+                if (line.contains(text)){
                     return num;
-                }
-                else{
+                } else {
                     num++;
                 }
              }
@@ -35,7 +33,11 @@ public class FileSearch {
             System.err.println("Caught"+e);
         }finally {
             if (inputStream != null) {
-                inputStream.close();
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             if (sc != null) {
                 sc.close();
@@ -45,7 +47,7 @@ public class FileSearch {
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
         System.out.println("结果行号：" + grep(new File(projectDir, "log.txt"), "BBB"));
     }
