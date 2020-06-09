2b1dc079a7597d73da029a3ee23be8e403ba49ea
Lion <756433556@qq.com>
2020-06-09 13:04:59 +0800

    在文件中查找匹配的行号 修改

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -8,7 +8,7 @@ public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {
+    public static int grep(File target, String text) throws RuntimeException {
         int Number = 1;
         try {
             Scanner s = new Scanner(target);
@@ -20,7 +20,7 @@ public class FileSearch {
                 ++Number;
             }
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            new RuntimeException();
         }
         return -1;
     }
