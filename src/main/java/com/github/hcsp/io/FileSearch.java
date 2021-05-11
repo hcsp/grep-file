034d28cfefcfad8df7ceb8e029492a59e418a28e
AndrewZ <zeq_96@163.com>
2021-05-11 17:03:14 +0800

    读取文件并返回行号

--- src/main/java/com/github/hcsp/io/FileSearch.java
+++ src/main/java/com/github/hcsp/io/FileSearch.java
@@ -1,12 +1,51 @@
 package com.github.hcsp.io;
 
-import java.io.File;
+import java.io.*;
 
 public class FileSearch {
     // 找到第一个包含text的行的行号，行号从1开始计算。若没找到，则返回-1。
     // 如果指定的文件不存在或者无法被读取，抛出一个IllegalArgumentException。
     // 请不要让这个方法抛出checked exception
-    public static int grep(File target, String text) {}
+    public static int grep(File target, String text) {
+        int result = -1;
+        if (!target.exists()) {
+            throw new IllegalArgumentException();
+        }
+        FileReader fileReader = null;
+        BufferedReader reader = null;
+        try {
+            fileReader = new FileReader(target);
+            reader = new BufferedReader(fileReader);
+            String line;
+            int temp = 0;
+            while ((line = reader.readLine()) != null) {
+                temp++;
+                if (line.trim().equals(text)) {
+                    result = temp;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         File projectDir = new File(System.getProperty("basedir", System.getProperty("user.dir")));
